c2082b8997b1f96423a44d680cce032405b1e0cd
Johnnieboah <johnnieestep21@gmail.com>
2016-01-21 01:54:37 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
       botRegex = /^\/cool guy/;  botRegexDL = /^\/PL/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/
       botRegexAd=/^\/vance/;botRegexMAJ = /^\/maj/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; 
-      botRegexCC = /^\/cc/; botRegexSR = /^\/hl rules/; botRegexYT = /^\/yt/;botRegexDL = /^\/DL/; botRegexdL = /^\/dl/; botRegexDL = /^\/Dl/;
+      botRegexCC = /^\/cc/; botRegexSR = /^\/hl rules/; botRegexYT = /^\/yt/; botRegexdd = /^\/daddy/; botRegexDD = /^\/Daddy/;
     
       botRegexSiege = /^\/siege/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
@@ -119,7 +119,12 @@ function respond() {
     postMessage("http://www.daddyleagues.com/pm");
     this.res.end();
   }
-          else if(request.text && botRegexDl.test(request.text)) {
+          else if(request.text && botRegexDD.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.daddyleagues.com/pm");
+    this.res.end();
+  }
+            else if(request.text && botRegexdd.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://www.daddyleagues.com/pm");
     this.res.end();
