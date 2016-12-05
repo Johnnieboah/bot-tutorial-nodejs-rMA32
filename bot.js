636722fa0737de7332706423e26cbff7cc870e7b
Johnnieboah <johnnieestep21@gmail.com>
2016-12-05 02:10:42 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -108,14 +108,14 @@ function respond() {
   else if(request.text && botRegexSC.test(request.text)) {
     this.res.writeHead(200);
     
-    postMessage("http://daddyleagues.com/maddenall32/team/"+request.text.substring(5,8)+"/schedule");
+    postMessage("http://daddyleagues.com/pure/team/"+request.text.substring(5,8)+"/schedule");
     this.res.end();
   }
   else if(request.text && botRegexP.test(request.text)) {
     this.res.writeHead(200);
     var req = request.text.substring(5,request.text.length);
     var rep = req.replace(/ /,"+");
-    postMessage("http://daddyleagues.com/maddenall32/players?name="+rep+"&position=all&team=all");
+    postMessage("http://daddyleagues.com/pure/players?name="+rep+"&position=all&team=all");
     
     this.res.end();
   }  
@@ -127,7 +127,7 @@ function respond() {
   } 
   else if(request.text && botRegexSb.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.reddit.com/r/maddenall32");
+    postMessage("http://www.reddit.com/r/pure");
     this.res.end();
   } 
   else if(request.text && botRegexSh.test(request.text)) {
